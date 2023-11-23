a46afd18bbc88e79a44b3c06f8fd243639ce7bae
Kazak74 <anich.email@gmail.com>
2023-11-23 19:21:22 +0300

    Module5 form added 1

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,6 +1,7 @@
 from django.http import Http404
 from django.shortcuts import render, redirect
 from MainApp.models import Snippet
+from MainApp.forms import SnippetForm
 
 
 def index_page(request):
@@ -9,8 +10,20 @@ def index_page(request):
 
 
 def add_snippet_page(request):
-    context = {'pagename': 'Добавление нового сниппета'}
-    return render(request, 'pages/add_snippet.html', context)
+    if request.method == 'GET': # Получаем чистую форму
+        form = SnippetForm()
+        context = {
+            'pagename': 'Добавление нового сниппета',
+            'form': form       
+        }
+        return render(request, 'pages/add_snippet.html', context)
+    
+    if request.method == 'POST': # хотим сохраить данные из формы
+        form = SnippetForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('snippets-list')
+        return render(request, 'pages/add_snippet.html', {'form': form})
 
 
 def snippets_page(request):
@@ -21,6 +34,8 @@ def snippets_page(request):
         'snippets': snippets
         }
     return render(request, 'pages/view_snippets.html', context)
+    
+
 
 def snippet_detail(request, snippet_id):
     snippet = Snippet.objects.get(id = snippet_id)
@@ -30,11 +45,11 @@ def snippet_detail(request, snippet_id):
         }
     return render(request, 'pages/snippet_detail.html', context)
    
-def snippet_create(request):
-    if request.method == 'POST':
-        name = request.POST['name']
-        lang = request.POST['lang']
-        code = request.POST['code']   
-        snippet = Snippet(name=name, lang=lang, code=code)
-        snippet.save()
-        return redirect('snippets-list')
+# def snippet_create(request):
+#     if request.method == 'POST':
+#         name = request.POST['name']
+#         lang = request.POST['lang']
+#         code = request.POST['code']   
+#         snippet = Snippet(name=name, lang=lang, code=code)
+#         snippet.save()
+#         return redirect('snippets-list')
