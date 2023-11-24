42c66fe915889aa9a34bc22c8659e955cd9b1eaf
Kazak74 <anich.email@gmail.com>
2023-11-24 15:20:47 +0300

    Add my snippets page

--- MainApp/views.py
+++ MainApp/views.py
@@ -12,6 +12,14 @@ def index_page(request):
     return render(request, 'pages/index.html', context)
 
 @login_required
+def my_snippets(request):
+    context = {'pagename': 'Мои сниппеты'}
+    snippets = Snippet.objects.filter(user = request.user)
+    context['snippets'] = snippets
+    return render(request, 'pages/view_snippets.html', context)
+
+
+@login_required
 def add_snippet_page(request):
     if request.method == 'GET': # Получаем чистую форму
         form = SnippetForm()
@@ -111,4 +119,4 @@ def login(request):
     
 def logout(request):
     auth.logout(request)
-    return redirect(request.META.get('HTTP_REFERER', '/'))
+    return redirect('home')
